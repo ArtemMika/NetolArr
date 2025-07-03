e7adab21b5ef9fc7e7b5efa844a28a5e7cd42602
Artem <artemmikgaz@yandex.ru>
2025-07-04 01:24:45 +0300

    Task Function

---
+++ Function_P2/Function_P2/Function_P2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+void Func(int value, int power, int result);
+
+int main(int argc, char** argv)
+{
+	setlocale(LC_ALL, "rus");
+
+	Func(5, 2, 1);
+	Func(3, 3, 1);
+	Func(4, 4, 1);
+}
+
+void Func(int value, int power, int result)
+{
+	for (int i = 0; i < power; i++)
+	{
+		result *= value;
+	}
+
+	std::cout << value << " в степени " << power << " = " << result << std::endl;
+}
---
+++ Function_P3/Function_P3/Function_P3.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+
+int fib_number(int n) 
+{
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    return fib_number(n - 1) + fib_number(n - 2);
+}
+
+void fibonacci(int count) 
+{
+    for (int i = 0; i < count; i++)
+    {
+        if (i > 0) cout << " ";
+        cout << fib_number(i);
+    }
+}
+
+int main() 
+{
+    setlocale(LC_ALL, "rus");
+
+    int value;
+    cout << "Введите число: ";
+    cin >> value;
+    cout << "Числа Фибоначчи: ";
+
+    fibonacci(value);
+
+    cout << endl;
+    return 0;
+}
