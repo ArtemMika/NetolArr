350fd1308a0f219db74e3f31189bedb56c9f4920
Artem <artemmikgaz@yandex.ru>
2025-07-05 17:55:46 +0300

    added task namespace

---
+++ Модуль Область видимости переменных и типы памяти. Пространства имен/Task_1/Task_1/Task_1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+void counting_function();
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    for (int i = 0; i < 15; i++)
+    {
+        counting_function();
+    }
+
+    return 0;
+}
+
+void counting_function()
+{
+    static int count{};
+    std::cout << "Количество вызовов функции counting_function(): " << ++count << std::endl;
+}
---
+++ Модуль Область видимости переменных и типы памяти. Пространства имен/Task_2/Task_2/Task_2.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+
+namespace Calculator 
+{
+    namespace Addition
+    {
+        int calc(int x, int y)
+        {
+            return x + y;
+        }
+    }
+    namespace Subtraction
+    {
+        int calc(int x, int y)
+        {
+            return x - y;
+        }
+    }
+    namespace Multiplication
+    {
+        int calc(int x, int y)
+        {
+            return x * y;
+        }
+    }
+    namespace Division
+    {
+        int calc(int x, int y)
+        {
+            if (y == 0) return 0;
+            return x / y;
+        }
+    }
+}
+
+int main() 
+{
+    setlocale(LC_ALL, "rus");
+
+    int x = 6;
+    int y = 9;
+
+    std::cout << "x = " << x << ", y = " << y << std::endl;
+
+    std::cout << "Сложение: " << Calculator::Addition::calc(x, y) << std::endl;
+    std::cout << "Вычитание: " << Calculator::Subtraction::calc(x, y) << std::endl;
+    std::cout << "Умножение: " << Calculator::Multiplication::calc(x, y) << std::endl;
+    std::cout << "Деление: " << Calculator::Division::calc(x, y) << std::endl;
+
+    return 0;
+}
+
+
