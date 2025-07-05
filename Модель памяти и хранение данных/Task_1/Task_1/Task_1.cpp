943e9908bc7ec334db5c3f6604acf96963151a62
Artem <artemmikgaz@yandex.ru>
2025-07-05 03:02:07 +0300

    added task memory model and data storage

---
+++ Модель памяти и хранение данных/Task_1/Task_1/Task_1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	int a;
+	short b;
+	long c;
+	long long d;
+	float i;
+	double f;
+	long double g;
+	bool t;
+
+	std::cout << "int: " << &a << "  " << sizeof(a) << std::endl;
+	std::cout << "short: " << &b << "  " << sizeof(b) << std::endl;
+	std::cout << "long: " << &c << "  " << sizeof(c) << std::endl;
+	std::cout << "long long: " << &d << "  " << sizeof(d) << std::endl;
+	std::cout << "float: " << &i << "  " << sizeof(i) << std::endl;
+	std::cout << "double: " << &f << "  " << sizeof(f) << std::endl;
+	std::cout << "long double: " << &g << "  " << sizeof(g) << std::endl;
+	std::cout << "bool: " << &t << "  " << sizeof(t) << std::endl;
+
+	return 0;
+}
---
+++ Модель памяти и хранение данных/Task_2/Task_2/Task_2.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+void swap(int& a, int& b);
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    int a = 5, b = 8;
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+
+    swap(a, b);
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+
+    return 0;
+}
+
+void swap(int& a, int& b)
+{
+    int c;
+    c = a;
+    a = b;
+    b = c;
+}
+//усложнения:
+/*void swap(int& a, int& b) 
+{
+    a = a + b;
+    b = a - b;
+    a = a - b;
+}
+*/
+/*
+void swap(int& a, int& b)
+{
+    a ^= b;
+    b ^= a;
+    a ^= b;
+}
+*/
