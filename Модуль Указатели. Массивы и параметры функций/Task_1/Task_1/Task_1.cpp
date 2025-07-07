07020b16530f808ac1a2a8c08809e45b152f86bc
Artem <artemmikgaz@yandex.ru>
2025-07-07 11:08:12 +0300

    correction task1

--- Модуль Указатели. Массивы и параметры функций/Task_1/Task_1/Task_1.cpp
+++ Модуль Указатели. Массивы и параметры функций/Task_1/Task_1/Task_1.cpp
@@ -9,14 +9,16 @@ int main()
 	int arr2[]{ 6, 5, 4, 8 };
 	int arr3[]{ 1, 4, 3, 7, 5 };
 
-	print(arr1,);
+	print(arr1, sizeof(arr1) / sizeof(arr1[0]));
+	print(arr2, sizeof(arr2) / sizeof(arr2[0]));
+	print(arr3, sizeof(arr3) / sizeof(arr3[0]));
 }
 
 void print(int* arr, int size)
 {
-	for (int value{}; value < size; ++value)
+	for (int counter{}; counter < size; ++counter)
 	{
-		std::cout << arr[value] << " ";
+		std::cout << arr[counter] << " ";
 	}
 	std::cout << std::endl;
 }
