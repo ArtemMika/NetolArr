cc000d6db9cc81a45bd657db2264f6b0f3b1c37e
Artem <artemmikgaz@yandex.ru>
2025-07-10 18:05:28 +0300

    Added Task-Lines

---
+++ Модуль Строки/Task_1/Task_1/Task_1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include<string>
+#include <windows.h>
+
+int main()
+{
+	SetConsoleCP(1251);// только для того чтобы можно было
+	SetConsoleOutputCP(1251);// писать и выводить на RUS
+
+	std::string name;
+	std::string surname;
+	std::cout << "Введите имя: ";
+	std::cin >> name;
+	std::cout << "Введите имя : ";
+	std::cin >> surname;
+
+	std::string greeting = "Здравствуйте, " + name + " " + surname + "!";
+	std::cout << greeting << std::endl;
+
+	return 0;
+}
+
---
+++ Модуль Строки/Task_2/Task_2/Task_2.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include<string>
+#include <windows.h>
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	std::string secretWord = "малина";
+	std::string user;
+	bool isGuessed = false;
+
+    do 
+    {
+        std::cout << "Угадайте слово: ";
+        std::cin >> user;
+
+        if (user == secretWord) {
+            std::cout << "Правильно! Вы победили! Загаданное слово — " << secretWord << std::endl;
+            isGuessed = true;
+        }
+        else {
+            std::cout << "Неправильно" << std::endl;
+        }
+    } while (!isGuessed);
+
+    return 0;
+}
