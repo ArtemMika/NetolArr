32123ce7b95f4c90627582d57d56f88c8293c795
Artem <artemmikgaz@yandex.ru>
2025-10-27 16:25:00 +0300

    added CMake

--- Модуль Сборка и подключение библиотек/LeaverLibrary/Leaver.cpp
+++
@@ -1,9 +0,0 @@
-#include "Leaver.h"
-
-namespace LeaverNamespace 
-{
-    std::string Leaver::leave(const std::string& name)
-    {
-        return "До свидания, " + name + "!";
-    }
-}
---
+++ Модуль Сборка и подключение библиотек/Task_3/Library/leaver.h
@@ -0,0 +1,12 @@
+#ifndef LEAVER_H
+#define LEAVER_H
+
+#include <string>
+
+class Leaver
+{
+public:
+    std::string leave(const std::string& name);
+};
+
+#endif
