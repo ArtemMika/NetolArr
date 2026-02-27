a8cc0c1c611e410750171d3ac1082449fd282a7d
Artem <artemmikgaz@yandex.ru>
2026-02-27 16:08:31 +0300

    Added greed

---
+++ Модуль Динамическое программирование и “жадность”/Task_1/Task_1.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <locale>
+
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
+void setup_locale() {
+#ifdef _WIN32
+    setlocale(LC_ALL, "Russian");
+    SetConsoleOutputCP(CP_UTF8);
+#else
+    setlocale(LC_ALL, "ru_RU.UTF-8");
+#endif
+}
+
+long long fibonacci(int n) {
+    if (n <= 1) {
+        return n;
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main() {
+    setup_locale();
+
+    int n;
+    std::cout << "Введите номер числа Фибоначчи (n): ";
+    std::cin >> n;
+
+    if (n < 0) {
+        std::cout << "Ошибка: n должно быть неотрицательным числом." << std::endl;
+        return 1;
+    }
+
+    long long result = fibonacci(n);
+    std::cout << "F(" << n << ") = " << result << std::endl;
+
+    return 0;
+}
---
+++ Модуль Динамическое программирование и “жадность”/Task_2/Task_2.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <vector>
+#include <locale>
+
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
+void setup_locale() {
+#ifdef _WIN32
+    setlocale(LC_ALL, "Russian");
+    SetConsoleOutputCP(CP_UTF8);
+#else
+    setlocale(LC_ALL, "ru_RU.UTF-8");
+#endif
+}
+
+std::vector<long long> memo;
+
+long long fibonacci_dp(int n) {
+    if (n <= 1) {
+        return n;
+    }
+
+    if (memo[n] != -1) {
+        return memo[n];
+    }
+
+    memo[n] = fibonacci_dp(n - 1) + fibonacci_dp(n - 2);
+    return memo[n];
+}
+
+int main() {
+    setup_locale();
+
+    int n;
+    std::cout << "Введите номер числа Фибоначчи (n): ";
+    std::cin >> n;
+
+    if (n < 0) {
+        std::cout << "Ошибка: n должно быть неотрицательным числом." << std::endl;
+        return 1;
+    }
+
+    memo.assign(n + 1, -1);
+    long long result = fibonacci_dp(n);
+    std::cout << "F(" << n << ") = " << result << std::endl;
+
+    return 0;
+}
