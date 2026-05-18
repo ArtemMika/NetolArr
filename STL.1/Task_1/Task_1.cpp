b7e44e023446d97eefaa6558c401e2cc1a00bd68
Artem <artemmikgaz@gmail.com>
2026-05-18 20:51:19 +0300

    added STL1

---
+++ STL.1/Task_1/Task_1.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void countAndSortCharacters(const string& text)
+{
+    map<char, int> frequency;
+    for (char c : text)
+    {
+        frequency[c]++;
+    }
+
+    vector<pair<char, int>> sortedFreq(frequency.begin(), frequency.end());
+
+    sort(sortedFreq.begin(), sortedFreq.end(),
+        [](const pair<char, int>& a, const pair<char, int>& b) 
+        {
+            return a.second > b.second;
+        });
+
+    cout << "[OUT]:" << endl;
+    for (const auto& pair : sortedFreq) 
+    {
+        cout << pair.first << ": " << pair.second << endl;
+    }
+}
+
+int main() 
+{
+    string input = "Hello world!!";
+    cout << "[IN]: " << input << endl;
+    countAndSortCharacters(input);
+
+    return 0;
+}
---
+++ STL.1/Task_2/Task_2.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main() 
+{
+    int n;
+    cin >> n;
+
+    set<int, greater<int>> uniqueNumbers;
+
+    for (int i = 0; i < n; ++i) 
+    {
+        int number;
+        cin >> number;
+        uniqueNumbers.insert(number);
+    }
+
+    cout << "[OUT]:" << endl;
+    for (const auto& num : uniqueNumbers) 
+    {
+        cout << num << endl;
+    }
+
+    return 0;
+}
