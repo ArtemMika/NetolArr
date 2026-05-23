e7bfa191b02c7db0a6f22a1f37bbf10a732ed62a
Artem <artemmikgaz@gmail.com>
2026-05-23 14:13:11 +0300

    added STL2

---
+++ STL.2/Task_1/Task_1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+void removeDuplicates(std::vector<int>& vec) 
+{
+    std::sort(vec.begin(), vec.end());
+
+    auto last = std::unique(vec.begin(), vec.end());
+
+    vec.erase(last, vec.end());
+}
+
+int main() 
+{
+    std::vector<int> data = { 1, 1, 2, 5, 6, 1, 2, 4 };
+
+    std::cout << "До удаления дубликатов: ";
+    for (int num : data) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+
+    removeDuplicates(data);
+
+    std::cout << "После удаления дубликатов: ";
+    for (int num : data) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
---
+++ STL.2/Task_2/Task_2.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <vector>
+#include <list>
+#include <set>
+#include <string>
+
+template<typename Container>
+void print_container(const Container& container)
+{
+    bool is_first = true;
+
+    for (const auto& element : container) 
+    {
+        if (!is_first) 
+        {
+            std::cout << ", ";
+        }
+        std::cout << element;
+        is_first = false;
+    }
+    std::cout << std::endl;
+}
+
+int main() 
+{
+    setlocale(LC_ALL, "Rus");
+
+    std::set<std::string> test_set = { "один", "два", "три", "четыре" };
+    std::cout << "std::set: ";
+    print_container(test_set);
+
+    std::list<std::string> test_list = { "один", "два", "три", "четыре" };
+    std::cout << "std::list: ";
+    print_container(test_list);
+
+    std::vector<std::string> test_vector = { "один", "два", "три", "четыре" };
+    std::cout << "std::vector: ";
+    print_container(test_vector);
+
+    return 0;
+}
