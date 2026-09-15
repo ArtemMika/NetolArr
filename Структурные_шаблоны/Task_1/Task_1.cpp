345d6716076d3c9c397d3a0610e7a7e1c62395f8
Artem <artemmikgaz@gmail.com>
2026-09-15 13:03:32 +0300

    Добавлены структуры шаблоны

---
+++ Структурные_шаблоны/Task_1/Task_1.cpp
@@ -0,0 +1,97 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+
+class TextComponent 
+{
+public:
+    virtual ~TextComponent() = default;
+    virtual void render(const std::string& text) const = 0;
+};
+
+class Text : public TextComponent
+{
+public:
+    void render(const std::string& text) const override 
+    {
+        std::cout << text;
+    }
+};
+
+class Paragraph : public TextComponent
+{
+public:
+    explicit Paragraph(TextComponent* component) : component_(component) {}
+
+    void render(const std::string& text) const override 
+    {
+        std::cout << "<p>";
+        component_->render(text);
+        std::cout << "</p>";
+    }
+
+private:
+    TextComponent* component_;
+};
+
+class Reversed : public TextComponent
+{
+public:
+    explicit Reversed(TextComponent* component) : component_(component) {}
+
+    void render(const std::string& text) const override 
+    {
+        std::string reversed = text;
+        std::reverse(reversed.begin(), reversed.end());
+        component_->render(reversed);
+    }
+
+private:
+    TextComponent* component_;
+};
+
+class Link 
+{
+public:
+    explicit Link(TextComponent* component) : component_(component) {}
+
+    void render(const std::string& url, const std::string& text) const
+    {
+        std::cout << "<a href=" << url << ">";
+        component_->render(text);
+        std::cout << "</a>";
+    }
+
+private:
+    TextComponent* component_;
+};
+
+int main() 
+{
+    // Paragraph
+    auto text_block = new Paragraph(new Text());
+    text_block->render("Hello world");
+    std::cout << "\n";
+
+    // Reversed
+    auto reversed_block = new Reversed(new Text());
+    reversed_block->render("Hello world");
+    std::cout << "\n";
+
+    // Link
+    auto link_block = new Link(new Text());
+    link_block->render("netology.ru", "Hello world");
+    std::cout << "\n";
+
+    // Комбинированный пример: ссылка с обратным текстом
+    auto link_reversed = new Link(new Reversed(new Text()));
+    link_reversed->render("netology.ru", "Hello world");
+    std::cout << "\n";
+
+    delete text_block;
+    delete reversed_block;
+    delete link_block;
+    delete link_reversed;
+
+    return 0;
+}
---
+++ Структурные_шаблоны/Task_2/Task_2.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <cstddef>
+#include <string>
+#include <unordered_map>
+
+class VeryHeavyDatabase 
+{
+public:
+    virtual ~VeryHeavyDatabase() = default;
+
+    virtual std::string GetData(const std::string& key) 
+    {
+        // поиск в «базе»
+        static const std::unordered_map<std::string, std::string> data = 
+        {
+            {"key", "value"},
+            {"other", "other_value"}
+        };
+        auto it = data.find(key);
+        return (it != data.end()) ? it->second : "not_found";
+    }
+};
+
+class OneShotDB : public VeryHeavyDatabase
+{
+public:
+    explicit OneShotDB(VeryHeavyDatabase* real_object, size_t shots = 1)
+        : real_object_(real_object), remaining_shots_(shots) 
+    {
+    }
+
+    std::string GetData(const std::string& key) override
+    {
+        if (remaining_shots_ == 0) 
+        {
+            return "error";
+        }
+
+        std::string result = real_object_->GetData(key);
+        --remaining_shots_;
+        return result;
+    }
+
+private:
+    VeryHeavyDatabase* real_object_;
+    size_t remaining_shots_;
+};
+
+int main()
+{
+    VeryHeavyDatabase real_db;
+    OneShotDB limit_db(&real_db, 2);
+
+    std::cout << limit_db.GetData("key") << "\n";  // value
+    std::cout << limit_db.GetData("key") << "\n";  // value
+    std::cout << limit_db.GetData("key") << "\n";  // error
+
+    return 0;
+}
